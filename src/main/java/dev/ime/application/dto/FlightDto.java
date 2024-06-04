13f62ba166839d5a6d200dfbf4473657d3386e1f
IvanM <ivan.montes@outlook.com>
2024-06-04 08:14:45 +0200

    feat: add validation

--- src/main/java/dev/ime/application/dto/FlightDto.java
+++ src/main/java/dev/ime/application/dto/FlightDto.java
@@ -4,13 +4,13 @@ import java.util.Set;
 import java.util.UUID;
 
 import dev.ime.application.config.ApplicationConstant;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record FlightDto(
 		UUID flightId,
-		@NotEmpty @Size( min = 1, max = 50 ) String origin,
-		@NotEmpty @Size( min = 1, max = 50 ) String destiny,
+		@NotBlank @Size( min = 1, max = 50 ) String origin,
+		@NotBlank @Size( min = 1, max = 50 ) String destiny,
 		Set<ClientDto> clientSet
 		) {
 	
