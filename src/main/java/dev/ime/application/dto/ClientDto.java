66809ee8220fce67efc8501176e08f12c69d6327
IvanM <ivan.montes@outlook.com>
2024-06-04 08:13:19 +0200

    feat: add validation

--- src/main/java/dev/ime/application/dto/ClientDto.java
+++ src/main/java/dev/ime/application/dto/ClientDto.java
@@ -3,12 +3,12 @@ package dev.ime.application.dto;
 import java.util.UUID;
 
 import dev.ime.application.config.ApplicationConstant;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ClientDto(
 		UUID clientId,
-		@NotEmpty @Size( min = 1, max = 100 ) String name
+		@NotBlank @Size( min = 1, max = 100 ) String name
 		) {
 	
 	public ClientDto() {
